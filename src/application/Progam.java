7347f392d8c4a1c347c810835f1d0c495f482079
Gabriel Milani <gabrielmilanib@gmail.com>
2024-03-11 23:25:20 -0300

    Class Position as created

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,7 +1,13 @@
 package application;
 
+import boardGame.Position;
+
 public class Progam {
 	public static void main (String[]args) {
-		System.out.println("Hello world!");
+		
+		Position pos = new Position (3,5);
+		
+		System.out.println(pos.toString());
+
 	}
 }
